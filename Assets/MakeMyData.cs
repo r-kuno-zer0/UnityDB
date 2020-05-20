97fc5c57e5eee96ae1a877bd0a473c2f0ecc3c8f
Ryo KUNO <kuno@aise.ics.saitama-u.ac.jp>
2020-05-20 12:25:49 +0900

    fisrt commit

---
+++ Assets/MakeMyData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeMyData : MonoBehaviour
+{
+    public GameObject test_text = null;
+    private MyData data;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            data = ScriptableObject.CreateInstance<MyData>();
+            data.myName = "Ryo";
+            data.hp = Random.Range(10, 50);
+            data.attackPower = 10f;
+            Text text = test_text.GetComponent<Text>();
+            text.text = data.myName + " : " + data.hp + " : " + data.attackPower;
+            Debug.Log(data.myName + " : " + data.hp + " : " + data.attackPower);
+        }else if (Input.GetButtonDown("Fire2"))
+        {
+            
+            Debug.Log(data.myName + " : " + data.hp + " : " + data.attackPower);
+        }
+    }
+}
---
+++ Assets/MyData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "Data")]
+public class MyData : ScriptableObject
+{
+
+    public string myName;
+    public int hp;
+    public float attackPower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
